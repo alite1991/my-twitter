ea80edcf629d9224eea0d66284d51cfe42247452
Alireza Tehrani <alite1991@users.noreply.github.com>
2021-10-07 20:03:50 +0330

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <h1>Hello Twitter</h1>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
